da07cd4601dcacd0e5d2ebc7b4e3bde1b40b03b0
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-06-20 09:52:50 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -62,9 +62,11 @@ router.post('/signin', async (req, res) => {
         res.cookie('jwtoken', token, {
             expires: new Date(Date.now() + 25892000000), // Expires in 30 days
             httpOnly: true, // Set to true to enable HTTP-only cookie
-            secure: true', // Set secure to true in production
-            sameSite: 'None'
+            secure: process.env.NODE_ENV === 'production', // Set secure to true in production
+            sameSite: 'None' // Ensure this matches your environment setup
         });
+
+        console.log('Token saved in cookie:', token);
 
         res.status(200).json({ message: 'User signed in successfully', token: token });
     } catch (error) {
