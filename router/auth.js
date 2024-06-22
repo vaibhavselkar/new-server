1bd4459b1b02ba7ecfa30c60e156a2219ae2070f
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-06-22 15:00:23 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -61,7 +61,7 @@ router.post('/signin', async (req, res) => {
             expires: new Date(Date.now() + 25892000000), // Approximately 30 days
             httpOnly: true,   // Prevents client-side JavaScript from accessing the cookie
             secure: true, // Ensures the cookie is sent over HTTPS only in production
-            sameSite: 'lax',
+            sameSite: 'none',
         });
 
         res.json({ message: 'Login successful' });
