4dabf2bf688ad07ffea84836c7aeff3de5bfb614
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-05 11:31:55 +0530

    Update auth.js

--- router/auth.js
+++ router/auth.js
@@ -61,6 +61,7 @@ router.post('/signin', async (req, res) => {
         console.log('Generated token:', token);
 
         res.cookie('jwtoken', token, {
+            path: '/',
             secure: true,
             httpOnly: true,
             sameSite: 'None',
