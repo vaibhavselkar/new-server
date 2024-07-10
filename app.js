e6b59e99854f05790117e108c10740e9d95cff4c
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-10 15:35:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    console.log('Session config:', req.session.cookie);
+    next();
+});
+
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
-    saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
     cookie: {
         httpOnly: true,
