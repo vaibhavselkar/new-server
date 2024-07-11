09a9d483bed642c09e56facf5e8c21060dd69b12
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-07-11 14:55:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }));
 app.use(cookieParser());
-app.options('*', cors(corsOptions)); // Handle preflight requests
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://sanghamitra-learning.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
